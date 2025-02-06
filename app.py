65efa40afe2810aa71d96f1c18a68fe210c66c52
Maud Gesbert <gesbertma@gmail.com>
2025-02-06 22:59:04 +0100

    PyA_mysite_joblib_dataframeformat

--- app.py
+++ app.py
@@ -18,29 +18,29 @@ FEATURE_NAMES = [
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-
-    # Set display options to show all columns and rows
+    # Set Pandas options to show full DataFrame
+    import pandas as pd
     pd.set_option('display.max_columns', None)  # Show all columns
-    pd.set_option('display.width', None)        # Prevent line breaks, display everything in a single line
-    pd.set_option('display.max_rows', None)     # Show all rows (if needed, adjust depending on data size)
+    pd.set_option('display.width', None)        # Prevent line breaks
+    pd.set_option('display.max_rows', None)     # Show all rows
 
     prediction = None  # Default value
 
     if request.method == 'POST':
         try:
-            # Step 1: Retrieve form data and convert booleans to 1 or 0
+            # Step 1: Retrieve form data and clean it
             input_data = [
-                float(request.form['EXT_SOURCE_3']),
-                float(request.form['EXT_SOURCE_2']),
+                clean_input(request.form['EXT_SOURCE_3']),
+                clean_input(request.form['EXT_SOURCE_2']),
                 1 if request.form.get('NAME_EDUCATION_TYPE_Higher education') == 'on' else 0,  # Check if checked
-                int(request.form['CODE_GENDER']),
+                clean_input(request.form['CODE_GENDER']),
                 1 if request.form.get('NAME_EDUCATION_TYPE_Secondary / secondary special') == 'on' else 0,  # Check if checked
-                int(request.form['FLAG_DOCUMENT_3']),
-                float(request.form['AMT_REQ_CREDIT_BUREAU_HOUR']),
-                int(request.form['REGION_RATING_CLIENT']),
-                float(request.form['EXT_SOURCE_1']),
+                clean_input(request.form['FLAG_DOCUMENT_3']),
+                clean_input(request.form['AMT_REQ_CREDIT_BUREAU_HOUR']),
+                clean_input(request.form['REGION_RATING_CLIENT']),
+                clean_input(request.form['EXT_SOURCE_1']),
                 1 if request.form.get('NAME_INCOME_TYPE_Working') == 'on' else 0,  # Check if checked
-                int(request.form['FLAG_EMP_PHONE'])
+                clean_input(request.form['FLAG_EMP_PHONE'])
             ]
 
             # Convert input to DataFrame with feature names
