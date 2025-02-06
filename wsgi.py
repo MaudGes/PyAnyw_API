f0bd8acd3c013e8bdcf8698d1af8d81c54d5417c
Maud Gesbert <gesbertma@gmail.com>
2025-02-06 14:50:22 +0100

    PyA Paths_mysite_5

--- wsgi.py
+++ wsgi.py
@@ -2,13 +2,14 @@ import sys
 import os
 
 # Add your project to the sys.path
-project_home = '/home/home/MaudGes/mysite'
+project_home = '/home/MaudGes/mysite'
 if project_home not in sys.path:
     sys.path.append(project_home)
 
-# Activate your virtual environment
-activate_this = '/home/home/MaudGes/mysite/envp7/bin/activate'
-exec(open(activate_this).read(), dict(__file__=activate_this))
+# Add the virtualenv site-packages to sys.path
+venv_site_packages = '/home/MaudGes/mysite/envp7/lib/python3.10/site-packages'
+if venv_site_packages not in sys.path:
+    sys.path.append(venv_site_packages)
 
 # Import your Flask app
 from app import app as application
