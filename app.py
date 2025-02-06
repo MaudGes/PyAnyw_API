0608d7d6e78a82aac31e82d9a1adc9221afe648f
Maud Gesbert <gesbertma@gmail.com>
2025-02-06 21:45:25 +0100

    PyA_mysite_joblib_intissue

--- app.py
+++ app.py
@@ -26,14 +26,14 @@ def home():
             input_data = [
                 float(request.form['EXT_SOURCE_3']),
                 float(request.form['EXT_SOURCE_2']),
-                int(request.form.get('NAME_EDUCATION_TYPE_Higher education', 0)),  # Defaults to 0 if not checked
+                1 if request.form.get('NAME_EDUCATION_TYPE_Higher education') == 'on' else 0,  # Check if checked
                 int(request.form['CODE_GENDER']),
-                int(request.form.get('NAME_EDUCATION_TYPE_Secondary / secondary special', 0)),  # Defaults to 0 if not checked
+                1 if request.form.get('NAME_EDUCATION_TYPE_Secondary / secondary special') == 'on' else 0,  # Check if checked
                 int(request.form['FLAG_DOCUMENT_3']),
                 float(request.form['AMT_REQ_CREDIT_BUREAU_HOUR']),
                 int(request.form['REGION_RATING_CLIENT']),
                 float(request.form['EXT_SOURCE_1']),
-                int(request.form.get('NAME_INCOME_TYPE_Working', 0)),  # Defaults to 0 if not checked
+                1 if request.form.get('NAME_INCOME_TYPE_Working') == 'on' else 0,  # Check if checked
                 int(request.form['FLAG_EMP_PHONE'])
             ]
 
